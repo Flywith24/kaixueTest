480222f180607283167a9de8e0c995a0224bd635
Flywith24 <fly_with24@163.com>
2019-08-23 11:48:51 +0800

    lesson1

--- app/src/main/java/cn/bcbook/kaixuetest/MainActivity.kt
+++ app/src/main/java/cn/bcbook/kaixuetest/MainActivity.kt
@@ -1,12 +1,20 @@
 package cn.bcbook.kaixuetest
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.tv_text)
+        printViewId(textView)
+    }
+
+    private fun printViewId(view: View?) {
+        log(view?.id.toString())
     }
 }
---
+++ app/src/main/java/cn/bcbook/kaixuetest/Utils.kt
@@ -0,0 +1,13 @@
+package cn.bcbook.kaixuetest
+
+import android.util.Log
+
+/**
+ * @author yyz (杨云召)
+ * @date   2019/8/23
+ * time   11:40
+ * description
+ */
+fun log(msg: String) {
+    Log.i("yyz", msg)
+}
